4fa734703879893b6a65b5eedaafbf066a44a06b
Nataliia Korneiko <korneyko_ns@ukr.net>
2020-03-12 15:43:08 +0200

    Updates

---
+++ precache-manifest.77d713187e2e8b0514870e29bc601681.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b37b47422f0e3c00ff8ab0b8a9b532",
+    "url": "/react-warriors/index.html"
+  },
+  {
+    "revision": "b165199476b687e55d54",
+    "url": "/react-warriors/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "831d44664b29b318a4cd",
+    "url": "/react-warriors/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "b165199476b687e55d54",
+    "url": "/react-warriors/static/js/2.fb48d99d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-warriors/static/js/2.fb48d99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831d44664b29b318a4cd",
+    "url": "/react-warriors/static/js/main.dad18ce4.chunk.js"
+  },
+  {
+    "revision": "56cc476195b89324813b",
+    "url": "/react-warriors/static/js/runtime-main.a9a2f7cf.js"
+  }
+]);
